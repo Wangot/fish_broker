3d744dff8d292d4e51a0c3a9461066784869517e
Raymande Leano <raymandeleano@gmail.com>
2017-03-11 10:29:01 +0800

    Add nav and first page

--- react_app/components/Nav.jsx
+++ react_app/components/Nav.jsx
@@ -1,9 +1,14 @@
 var React = require('react');
+var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
   render: function () {
     return (
-      <h2>Nav Component</h2>
+      <div>
+        <h2>Nav Component</h2>
+        <IndexLink to="/privatespace" activeClassName="active">Dashboard</IndexLink>
+        <Link to="/privatespace/creditors" activeClassName="active">Creditors</Link>
+      </div>
     );
   }
 });
--- react_app/main.jsx
+++ react_app/main.jsx
@@ -1,15 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, browserHistory} = require('react-router');
+var {Route, Router, IndexRoute, browserHistory, hashHistory} = require('react-router');
 
 var MainLayout = require('./components/MainLayout');
 var DashBoard = require('./components/DashBoard');
+var Creditors = require('./components/Creditors');
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path="/privatespace" component={MainLayout}>
+    <Route path="privatespace" component={MainLayout}>
       <IndexRoute component={DashBoard} />
-      <Route path="creditors" component={DashBoard}/>
+      <Route path="creditors" component={Creditors}/>
     </Route>
   </Router>,
   document.getElementById('app')
